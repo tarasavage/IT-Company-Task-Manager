a447a2460ec3e152218e805c66298147e00ce699
Taras Panasiuk <107722931+tarasavage@users.noreply.github.com>
2023-04-12 21:58:46 +0300

    Installed black and used it

--- tasks/models.py
+++ tasks/models.py
@@ -41,9 +41,7 @@ class Task(models.Model):
         default="3",
     )
     task_type = models.ForeignKey(to=TaskType, on_delete=models.CASCADE)
-    assignees = models.ManyToManyField(
-        to=Worker, related_name="tasks", blank=True
-    )
+    assignees = models.ManyToManyField(to=Worker, related_name="tasks", blank=True)
 
     def __str__(self) -> str:
         return f"{self.name} {self.priority} {self.deadline} {self.is_completed}"
